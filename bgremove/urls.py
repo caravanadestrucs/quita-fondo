2364d5ae2112288d5f5c829bcf692d4c0d01535f
caravanadestrucs <li_djoaquin@unca.edu.mx>
2025-08-30 08:41:44 -0600

    commit robots

--- bgremove/urls.py
+++ bgremove/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from .views import remove_background, api_remove_background, index
+from django.views.static import serve
 
 
 urlpatterns = [
     path("", index, name="index"),
     path("remove/", remove_background, name="upload"),
     path("api/remove/", api_remove_background, name="api_remove"),
+    path("robots.txt", serve, {"path": "robots.txt", "document_root": "static"}, name="robots"),
+    path("sitemap.xml", serve, {"path": "sitemap.xml", "document_root": "static"}, name="sitemap"),
 ]
